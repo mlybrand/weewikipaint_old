03b6cd966c7d2f31e2c6f95848dcd1284edfe328
Mark Lybrand <mlybrand@gmail.com>
2013-05-24 22:22:14 -0700

    Created Node http.get spike

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,14 @@
 "use strict";
 
-exports.number = function() {
-    return 3;
+var http = require("http");
+
+exports.start = function() {
+    var server = http.createServer();
+
+    server.on("request", function(request, response) {
+        console.log("Received request");
+        response.end("foo");
+    });
+
+    server.listen(8080);    // TODO: remove duplication of port number
 };
