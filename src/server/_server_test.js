9daf874df507174ad9b66bb47a5299f7518a5c83
Mark Lybrand <mlybrand@gmail.com>
2013-05-31 01:43:55 -0700

    got first server/client test working

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -3,12 +3,17 @@
 var server = require("./server.js");
 var http = require("http");
 
-exports.testHttpServer = function(test) {
-    server.start();
-
-    http.get("http://localhost:8080", function(response) {
-
+exports.tearDown = function(done) {
+    server.stop(function() {
+        done();
     });
-    test.done();
+};
+
+exports.testServerRespondsToGetRequest = function(test) {
+    server.start();
+    http.get("http://localhost:8080", function(response) {
+        response.on("data", function() {});
+        test.done();
+    });
 };
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,18 @@
 "use strict";
 
 var http = require("http");
+var server;
 
 exports.start = function() {
-    var server = http.createServer();
+    server = http.createServer();
 
     server.on("request", function(request, response) {
-        console.log("Received request");
-        response.end("foo");
+        response.end();
     });
 
     server.listen(8080);    // TODO: remove duplication of port number
+};
+
+exports.stop = function(callback) {
+    server.close(callback);
 };
