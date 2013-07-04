9202a0480aa10006222e8f991e059aa6e3f0a154
Mark Lybrand <mlybrand@gmail.com>
2013-07-03 21:18:30 -0700

    We serve 404 page from file

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,35 @@
-"use strict";
+(function() {
+    "use strict";
 
-var http = require("http");
-var server;
+    var http = require("http");
+    var fs = require("fs");
+    var server;
 
-exports.start = function(portNumber) {
-    if (!portNumber) throw new Error("port number is required");
-    server = http.createServer();
-    server.on("request", function(request, response) {
-        response.end("Hello World");
-    });
-    server.listen(portNumber);
-};
+    exports.start = function(homePageToServe, notFoundPageToServe, portNumber) {
+        if (!portNumber) throw new Error("port number is required");
+        server = http.createServer();
+        server.on("request", function(request, response) {
+            if (request.url === "/" || request.url === "/index.html") {
+                response.statusCode = 200;
+                serveFile(response, homePageToServe);
+            } else {
+                response.statusCode = 404;
+                serveFile(response, notFoundPageToServe);
+            }
+        });
+        server.listen(portNumber);
+    };
 
-exports.stop = function(callback) {
-    server.close(callback);
-};
+    exports.stop = function(callback) {
+        server.close(callback);
+    };
+
+    function serveFile(response, file) {
+        fs.readFile(file, function (err, data) {
+            if (err) throw err;   // TODO: Fix me
+            response.end(data);
+        });
+    }
+
+}());
+
